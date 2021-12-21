d79b6c74506f594cc8c74ccd6804997797d8ffef
chesteraustin <chesteraustin@gmail.com>
2021-12-21 12:06:13 -0800

    second draft

--- christmas-2021/_next/static/3-cRZZUK9k8_tBVQAyAy9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/228-6cc19f5f77e2a4d1.js","static/css/cc7b86c275aaf0fb.css","static/chunks/pages/index-caef5228dd591d97.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ christmas-2021/_next/static/dKcYsDzlSITNvHgx8EfCl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/228-6cc19f5f77e2a4d1.js","static/css/cc7b86c275aaf0fb.css","static/chunks/pages/index-68bc76e94f0336b8.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
